6ce0e0720543867a62f4efe566209bee0fec0f45
Steven Rouk <stevenrouk@gmail.com>
2016-01-13 17:46:41 -0700

    add registration page and database-based user authentication

--- project/_config.py
+++ project/_config.py
@@ -9,8 +9,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'something-no-one-will-ever-guess'
 
--- project/db_create.py
+++ project/db_create.py
@@ -10,8 +10,8 @@ from datetime import date
 db.create_all()
 
 # insert dummy data
-db.session.add(Task("Finish this tutorial", date(2016, 1, 13), 10, 1))
-db.session.add(Task("Finish RP Course 2", date(2016, 2, 1), 7, 1))
+#db.session.add(Task("Finish this tutorial", date(2016, 1, 13), 10, 1))
+#db.session.add(Task("Finish RP Course 2", date(2016, 2, 1), 7, 1))
 
 # commit the changes
 db.session.commit()
